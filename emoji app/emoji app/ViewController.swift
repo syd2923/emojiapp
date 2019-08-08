aad7c11f4a28a053837797418143badbb9f4573a
Sydney Brown <sydneybrown@Schawns-MacBook-Pro.local>
2019-08-08 16:37:05 -0400

    emoji app

--- emoji app/emoji app/ViewController.swift
+++ emoji app/emoji app/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var andIoop: UIButton!
+    
+    @IBOutlet weak var rad: UIButton!
+    
+    @IBOutlet weak var bigMad: UIButton!
+    
+    
+    @IBOutlet weak var sad: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+func rad(_ sender: UIButton) {
+    showMessage(sender: UIButton) {
+        let selectionEmotion = sender.titleLabel?.text
+    let alertController = UIAlertController()
+    let alertController = UIAlertController.init(title: "😎", message: "you're rad", preferredStyle: UIAlertController.Style.alert)
+    
+   present(alertController, animated: true, completion: nil)
+    alertController.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+   
 
+let emojis = ["😎" : "rad", "😗" : "and i oop", "☹️" : "ugh", "🤬" : "major uggh"]
+}
 }
 
+
+}
