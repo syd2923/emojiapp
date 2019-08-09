3360933e29072e4b1e49694b1801215d547e3c07
Sydney Brown <sydneybrown@Schawns-MacBook-Pro.local>
2019-08-09 11:48:56 -0400

    emoji

--- emoji app/emoji app/ViewController.swift
+++ emoji app/emoji app/ViewController.swift
@@ -8,41 +8,116 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var andIoop: UIButton!
+class ViewController: UIViewController {override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
     
-    @IBOutlet weak var rad: UIButton!
-    
-    @IBOutlet weak var bigMad: UIButton!
+    let emojis = ["😎" : "rad", "😗" : "and i oop", "☹️" : "upset", "🤬" : "major uggh"]
     
     
-    @IBOutlet weak var sad: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let customMessages = ["rad": ["you're super rad", "keep wearing them glasses","spooky cool"], "and i oop" : ["thats the tea sis", "girlll so and so said!", "keep doing them kissy lips"], "upset" : ["call your mom", "eat a tub of ice cream"], "major uggh" : ["it's gonna be ok don't cry!", "you are stronger than that"]]
+    
+    @IBAction func andioop(_ sender: Any) {
+        let selectedEmotion =
+            (sender as AnyObject).titleLabel?.text
+        let random = Int.random(in : 0..<2)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+           // = customMessages.randomElement()!
+        
+        let alertController = UIAlertController.init(title: "and i oop", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        
+       
+        
+        present(alertController, animated: true, completion: nil)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
     }
     
     
- @IBAction func 😎(_ sender: UIButton) {
-    func rad(_ sender: UIButton) {
-    _ = sender.titleLabel?.text
-    let alertController = UIAlertController()
-    _ = UIAlertController.init(title: "😎", message: "you're rad", preferredStyle: UIAlertController.Style.alert)
-   
-    present(alertController, animated: true, completion: nil)
-        alertController.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+    @IBAction func rad(_ sender: Any) {
+        let selectedEmotion =
+            
+            (sender as AnyObject).titleLabel?.text
+        let random = Int.random(in : 0..<2)
         
-        let customMessage = ["That's awsome", "Keep it up", "You go girl"]
-
-        _ = UIAlertController.init(title: "rad", message: "\(customMessage.randomElement()!)", preferredStyle: UIAlertController.Style.alert)
+  let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+        _ = customMessages.randomElement()!
         
-   
+        let alertController = UIAlertController.init(title: "rad", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        
+        present(alertController, animated: true, completion: nil)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    }
+    
+    
+    
+    @IBAction func sadFace(_ sender: Any) {
+        let selectedEmotion = (sender as AnyObject).titleLabel?.text
+        let random = Int.random(in : 0..<2)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+        _ = customMessages.randomElement()!
+        
+        let alertController = UIAlertController.init(title: "upset", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        
+        present(alertController, animated: true, completion: nil)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        
+    }
+    @IBAction func madFace(_ sender: Any) {
+        let selectedEmotion =
+            
+            (sender as AnyObject).titleLabel?.text
+        let random = Int.random(in : 0..<2)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+        _ = customMessages.randomElement()!
+        
+        let alertController = UIAlertController.init(title: "major uggh", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        
+        present(alertController, animated: true, completion: nil)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        
+    }
+    
+//        @IBAction func madFace(_ sender: Any) {
+//        let selectedEmotion = (sender as AnyObject).titleLabel?.text
+//        let random = Int.random(in : 0..<2)
+//
+//         let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+//        _ = customMessages.randomElement()!
+//
+//        let alertController = UIAlertController.init(title: "major uggh", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+//
+//        present(alertController, animated: true, completion: nil)
+//
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        
+    }
 
-    _ = ["😎" : "rad", "😗" : "and i oop", "☹️" : "ugh", "🤬" : "major uggh"]
-}
-}
-}
+    
+    
+    //func rad(_ sender: UIButton) {
+    //    _ = sender.titleLabel?.text
+    //    let alertController = UIAlertController()
+    //
+    //
+    //    _ = UIAlertController.init(title: "rad", message: "you're rad", preferredStyle: UIAlertController.Style.alert)
+    //
+    //alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    //
+    // _ = UIAlertController.init(title: "rad", message: "\(customMessages.randomElement()!)", preferredStyle: UIAlertController.Style.alert)
+    //}
+    
 
 
